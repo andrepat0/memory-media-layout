d5eaed4313fd30a7754403e73352837dcf198ec0
andrepat0 <patini929@gmail.com>
2024-07-28 14:42:38 +0200

    feat(ui): added user logged in Side Panel and removed inputs to Chat inside Drawer

--- src/components/MemoriAi/CustomLayout/StatePanel/StatePanel.tsx
+++ src/components/MemoriAi/CustomLayout/StatePanel/StatePanel.tsx
@@ -5,12 +5,12 @@ import {
   ChatProps,
 } from "@memori.ai/memori-react/dist/components/MemoriWidget/MemoriWidget";
 import Drawer from "@memori.ai/memori-react/dist/components/ui/Drawer";
-import Chat from "@memori.ai/memori-react/dist/components/Chat/Chat"
+import Chat from "@memori.ai/memori-react/dist/components/Chat/Chat";
 
 export default function StatePanel({
   user,
   userLoggedin,
-  chatProps
+  chatProps,
 }: {
   user: StartPanelProps["user"];
   userLoggedin: StartPanelProps["isUserLoggedIn"];
@@ -24,28 +24,37 @@ export default function StatePanel({
 
   return (
     <div className="memori-panel--container">
-      <p className="memori-panel-logged-user--content">
-        {userLoggedin ? user : ""}
-      </p>
+      <div className="memori-panel-logged-user--container">
+        {user?.avatarURL ? <img src={user?.avatarURL} className="memori-panel-logged-user--avatar" />
+        : <div className="memori-panel-unlogged-user--avatar" >
+          <img style={{width: 35, margin: "auto"}} src="./user-icon.svg" alt="Utente non loggato" />
+          </div>}
+        <p className="memori-panel-logged-user--text">
+          {userLoggedin ? user?.userName : "Utente non loggato"}
+        </p>
+      </div> 
       <div className="memori-panel-user-text--container">
         <p className="memori-panel-user-text--title">Testo Inviato:</p>
-        <p className="memori-panel-user-text--content">
+        <p className="memori-panel-user-text--text">
           {lastUserText && lastUserText.length > 0
             ? lastUserText[lastUserText.length - 1]?.text
             : "Nessun testo inviato"}
         </p>
       </div>
-      <div className="memori-panel-user-text--container">
-        <button
-          onClick={() => setShowChat(!showChat)}
-          className="memori-panel-user-text--button"
-        >
-          Mostra Chat
-        </button>
-      </div>
+      <button
+        onClick={() => setShowChat(!showChat)}
+        className="memori-panel-user-text--button"
+      >
+        Mostra Chat
+      </button>
       <Drawer onClose={() => setShowChat(false)} open={showChat}>
         <div className="memori-panel-chat--container">
-          <Chat {...chatProps}  />
+          <Chat
+            showInputs={false}
+            memori={chatProps.memori}
+            sessionID={chatProps.sessionID}
+            history={chatProps.history}
+          />
         </div>
       </Drawer>
     </div>
